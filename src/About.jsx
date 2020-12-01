34ccecaff909d10060ca015222ed29a689025760
fabricio <carolinoreinaldo@gmail.com>
2020-12-01 09:50:30 -0300

    adição da navegação com o Link

--- src/About.jsx
+++ src/About.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function About() {
   return (
     <div className="App">
       <h1>About</h1>
+      <span><Link to="/">Ir para Home</Link></span>
     </div>
   );
 }
--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Home</h1>
+      <span><Link to="About">Ir para Sobre</Link></span>
     </div>
   );
 }
